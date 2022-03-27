ea7a149f0d6a185cf793d1b06534ba567dd72fdd
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-03-27 12:34:19 -0300

    added identity server with duende

---
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,55 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options =>
+    options.EnableSensitiveDataLogging(true)
+           .UseMySql(connection, new MySqlServerVersion(new Version(10, 6))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySqlContext>()
+    .AddDefaultTokenProviders();
+
+var builderIndetity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+  .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+  .AddInMemoryClients(IdentityConfiguration.Clients)
+  .AddAspNetIdentity<ApplicationUser>();
+
+builderIndetity.AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
