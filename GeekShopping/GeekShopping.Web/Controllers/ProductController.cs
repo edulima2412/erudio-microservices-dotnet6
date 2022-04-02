66697ae55044ecd60202aef37624cdc23e28b337
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-02 09:49:16 -0300

    added home page and controller

--- GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -16,7 +16,6 @@ namespace GeekShopping.Web.Controllers
             _productService = productService ?? throw new ArgumentNullException(nameof(productService));
         }
 
-        [Authorize]
         public async Task<IActionResult> ProductIndex()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
