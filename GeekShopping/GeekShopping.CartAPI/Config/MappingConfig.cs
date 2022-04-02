9f90348361ab710a6a3aeaf35d39e7d87c255109
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-02 10:33:10 -0300

    added micro-service of cart

---
+++ GeekShopping/GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace GeekShopping.CartAPI.Config
+{
+    public static class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductVO, Product>()
+                //    .ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.CartAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,27 @@
+﻿using GeekShopping.CartAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(decimal))))
+                property.SetColumnType("decimal(10,2)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MySqlContext).Assembly);
+
+            modelBuilder.Entity<Product>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
