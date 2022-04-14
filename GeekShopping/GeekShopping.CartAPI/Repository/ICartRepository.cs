58b010329ab7a2afbbc73656b4cbb5af410e3ede
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-14 09:10:44 -0300

    created repositories cart

---
+++ GeekShopping/GeekShopping.CartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartByUserId(Guid userId);
+        Task<CartVO> SaveOrUpdateCart(CartVO cart);
+        Task<bool> RemoveFromCart(Guid cartDetailsId);
+        Task<bool> ApplyCoupon(Guid userId, string CouponCode);
+        Task<bool> RemoveCoupon(Guid userId);
+        Task<bool> ClearCart(Guid userId);
+    }
+}
