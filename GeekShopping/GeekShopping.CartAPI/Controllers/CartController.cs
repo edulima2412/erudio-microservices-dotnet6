adc05fb6dfc6a82da9f5814e531fdc767d082eb4
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-14 09:11:18 -0300

    added scoped and created cart controller

---
+++ GeekShopping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.CartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _repository;
+
+        public CartController(ICartRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+    }
+}
--- GeekShopping/GeekShopping.CartAPI/Program.cs
+++ GeekShopping/GeekShopping.CartAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using GeekShopping.CartAPI.Config;
 using GeekShopping.CartAPI.Model.Context;
+using GeekShopping.CartAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -18,6 +19,8 @@ builder.Services.AddDbContext<MySqlContext>(options =>
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 builder.Services.AddControllers();
 
