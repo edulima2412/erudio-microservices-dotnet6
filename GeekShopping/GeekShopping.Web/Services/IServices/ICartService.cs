c036d1dbdcb31763d4144e8b7d7ccb6f4cc2cc84
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-14 09:37:48 -0300

    created cart service

---
+++ GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<CartViewModel> FindByCartUserId(Guid userId, string token);
+        Task<CartViewModel> AddItemToCart(CartViewModel cart, string token);
+        Task<CartViewModel> UpdateCart(CartViewModel cart, string token);
+        Task<bool> RemoveFromCart(Guid cartId, string token);
+        Task<bool> ApplyCoupon(CartViewModel cart, string couponCode, string token);
+        Task<bool> RemoveCoupon(Guid userId, string token);
+        Task<bool> ClearCart(Guid userId, string token);
+        Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token);
+    }
+}
