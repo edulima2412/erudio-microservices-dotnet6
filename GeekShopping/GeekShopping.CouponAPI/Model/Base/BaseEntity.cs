089bb0e982c4bd4c4f48d6555363bcc9fe7da773
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-15 08:40:12 -0300

    created micro service coupon api

---
+++ GeekShopping/GeekShopping.CouponAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CouponAPI.Model.Base
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        [Column("Id")]
+        public Guid Id { get; set; }
+    }
+}
---
+++ GeekShopping/GeekShopping.CouponAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(decimal))))
+                property.SetColumnType("decimal(10,2)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MySqlContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
