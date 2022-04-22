c7851984f17d64ae71176b406f655be8f2a7aa25
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-22 08:03:37 -0300

    added logic coupon

--- GeekShopping/GeekShopping.Web/Services/CartService.cs
+++ GeekShopping/GeekShopping.Web/Services/CartService.cs
@@ -54,10 +54,27 @@ namespace GeekShopping.Web.Services
                 throw new Exception("Something went wrong calling the API");
         }
 
-        public async Task<bool> ApplyCoupon(CartViewModel cart, string couponCode, string token)
+        public async Task<bool> ApplyCoupon(CartViewModel model, string token)
         {
-            throw new NotImplementedException();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PostAsJson($"{BasePath}/apply-coupon", model);
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else
+                throw new Exception("Something went wrong calling the API");
         }
+
+        public async Task<bool> RemoveCoupon(Guid userId, string token)
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.DeleteAsync($"{BasePath}/remove-coupon/{userId}");
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else
+                throw new Exception("Something went wrong calling the API");
+        } 
 
         public async Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
         {
@@ -68,10 +85,5 @@ namespace GeekShopping.Web.Services
         {
             throw new NotImplementedException();
         }
-
-        public async Task<bool> RemoveCoupon(Guid userId, string token)
-        {
-            throw new NotImplementedException();
-        } 
     }
 }
--- GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
+++ GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
@@ -8,7 +8,7 @@ namespace GeekShopping.Web.Services.IServices
         Task<CartViewModel> AddItemToCart(CartViewModel cart, string token);
         Task<CartViewModel> UpdateCart(CartViewModel cart, string token);
         Task<bool> RemoveFromCart(Guid cartId, string token);
-        Task<bool> ApplyCoupon(CartViewModel cart, string couponCode, string token);
+        Task<bool> ApplyCoupon(CartViewModel cart, string token);
         Task<bool> RemoveCoupon(Guid userId, string token);
         Task<bool> ClearCart(Guid userId, string token);
         Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token);
