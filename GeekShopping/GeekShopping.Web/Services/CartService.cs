23c4f2dc6716ab3ff0c0658b907c0f5776b30347
Eduardo Lima <carlos.rocha@compasso.com.br>
2022-04-22 09:07:29 -0300

    added implementation checkout

--- GeekShopping/GeekShopping.Web/Services/CartService.cs
+++ GeekShopping/GeekShopping.Web/Services/CartService.cs
@@ -76,9 +76,15 @@ namespace GeekShopping.Web.Services
                 throw new Exception("Something went wrong calling the API");
         } 
 
-        public async Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
+        public async Task<CartHeaderViewModel> Checkout(CartHeaderViewModel model, string token)
         {
-            throw new NotImplementedException();
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _client.PostAsJson($"{BasePath}/checkout", model);
+
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<CartHeaderViewModel>();
+            else
+                throw new Exception("Something went wrong calling the API");
         }
 
         public async Task<bool> ClearCart(Guid userId, string token)
